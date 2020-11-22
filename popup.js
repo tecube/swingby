2d6e9c5122d1fbcf11c93a8df7b02e6933a1707d
tecube <tecube3@gmail.com>
2020-11-22 16:45:58 +0900

    show stored words in the popup

--- popup.js
+++ popup.js
@@ -1,4 +1,18 @@
+function build_stored_word_list_html(items) {
+    var word_ul = document.getElementById("word-list");
+
+    for(headword in items) {
+        var li = document.createElement("li", {"id": "headword-" + headword});
+        var t = document.createTextNode(headword);
+        console.log(headword)
+        li.appendChild(t);
+        word_ul.appendChild(li);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+    chrome.storage.sync.get(null, build_stored_word_list_html);
+
     var clear_yes = document.getElementById("yes");
     clear_yes.style.display = "none";
     
@@ -18,10 +32,5 @@ document.addEventListener('DOMContentLoaded', function() {
     clear_yes.addEventListener("click", function(){
         chrome.storage.sync.clear();
         clear_yes.style.display ="none";
-    }, false);
-    
-    var option_link = document.getElementById("option");
-    option_link.addEventListener("click", function(){
-        chrome.tabs.create({ "url": "option.html" });
     }, false);
 });
